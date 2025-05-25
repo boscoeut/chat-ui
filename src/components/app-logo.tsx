a182abe91a055fd1ee387317ad01179e7a074ff0
boscoeut <boscoeut@gmail.com>
2025-05-25 14:33:21 -0600

    Update AppLogo component: enhance logo presentation with a rounded container and improved image styling for better aesthetics.

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -2,8 +2,10 @@ import { appConfig } from "@/config/app"
 
 export function AppLogo() {
     return (
-        <div className='flex items-center gap-2'>
-            <img src="/logo.jpg" alt="Logo" className="size-8" />
+        <div className="flex items-center gap-2">
+            <span className="inline-flex items-center justify-center border border-gray-300 rounded-xl bg-white/80 w-10 h-10 overflow-hidden">
+                <img src="/logo.jpg" alt="Logo" className="w-full h-full object-cover rounded-xl" />
+            </span>
             <span className="font-semibold text-nowrap text-2xl">{appConfig.name}</span>
         </div>
     )
